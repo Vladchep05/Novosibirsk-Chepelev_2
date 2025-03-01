3dbd9e80b326c21111d234dde3858afb3607860e
sanya <vladcepel05@gmail.com>
2025-03-02 01:30:12 +0700

    Сделал задание Git и желтые окружности

--- main.py
+++ main.py
@@ -1,27 +1,33 @@
 import sys
 import random
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QGraphicsScene, QGraphicsView, QGraphicsEllipseItem
-from PyQt5.uic import loadUi
-from PyQt5.QtCore import QRectF
+from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QBrush, QColor
 
 
 class CircleWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        loadUi('UI.ui', self)
+
+        self.setGeometry(100, 100, 800, 600)
+
+        self.pushButton = QPushButton('добавить круг', self)
+        self.pushButton.setGeometry(10, 10, 200, 40)
         self.pushButton.clicked.connect(self.add_circle)
 
         self.scene = QGraphicsScene(self)
-        self.graphicsView.setScene(self.scene)
+        self.graphicsView = QGraphicsView(self.scene, self)
+        self.graphicsView.setGeometry(0, 60, 800, 540)
 
     def add_circle(self):
         diameter = random.randint(20, 100)
-        x = random.randint(0, 500)
+        x = random.randint(0, 700)
         y = random.randint(0, 500)
 
+        color = QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
         circle = QGraphicsEllipseItem(x, y, diameter, diameter)
-        circle.setBrush(QBrush(QColor(255, 255, 0)))
+        circle.setBrush(QBrush(color))
         self.scene.addItem(circle)
 
 
